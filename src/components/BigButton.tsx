5c1cbdbeeffb4da18db72dc518359f7d688e74b4
Andrew Sanchez <asanchez@yext.com>
2023-05-30 12:25:53 -0400

    add complex prop

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -1,16 +1,28 @@
 export interface BigButtonProps {
   title?: string;
+  myComplex?: ComplexProp
+}
+
+export interface ComplexProp {
+  field1: string;
+  field2: string;
 }
 
 export const initialProps: BigButtonProps = {
   title: "Button Title",
-};
+  myComplex: {
+    field1: "field1",
+    field2: "field2"
+  }
+}
 
-const BigButton = ({ title }: BigButtonProps) => {
+
+
+const BigButton = ({ title, myComplex }: BigButtonProps) => {
   return (
     <div className="mt-10">
       <button className="flex w-full items-center justify-center rounded-md border border-transparent bg-sky-200 py-3 px-8 text-base font-medium text-white">
-        {title}
+        {title} {myComplex?.field1} {myComplex?.field2}
       </button>
     </div>
   );
